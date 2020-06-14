583ce32997a1df41924334bc634f3f9a41ac90c2
Donald <donald_langston@yahoo.com>
2020-06-14 00:43:15 -0400

    Update index.js and reducer.js to add and display name tags

--- nameTags/index.js
+++ nameTags/index.js
@@ -7,3 +7,17 @@ dispatch({ type: null }); // Here we're making a call to dispatch() - this trigg
 
 // Write DOM event listeners here, make them dispatch actions to the Redux store 
 
+let form = document.getElementById('nameForm');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //name stores first element of form (text box) when form is submitted
+    let name = e.target[0];
+    
+    if(name.value) {
+        dispatch({type: 'CREATE_TAG', name: name.value });
+        //set text input box back to empty after submitting, ready for next input
+        name.value = "";
+    }
+     
+});
--- nameTags/reducer.js
+++ nameTags/reducer.js
@@ -5,9 +5,16 @@ const initialState = [
 
 const reducer = (state = initialState, action) => {
     // Handle actions here - make sure you don't mutate the state!
-    const { type } = action;
+    const { type, name } = action;
 
     // Add a Name Tag
+    if(type === 'CREATE_TAG') {
+        let newState = state.map((item) => {
+            return item;
+        });
+        newState.push(name);
+        return newState;
+    }
 
     return state;
 }
