e34f6e35d32bed1574942d4fb92e73a9998ba0a6
Donald <donald_langston@yahoo.com>
2020-06-14 12:07:26 -0400

    Updated code in index.html index.js and reducer.js to sort tweets

--- tweets/reducer.js
+++ tweets/reducer.js
@@ -35,18 +35,46 @@ const initialState = [
         retweets: 3,
         replies: 144
     }
-]
-;
+];
+
+function sortTweets(state, param, direction) {
+    let tweetArray = state.map((tweet) => {
+        return tweet;
+    });
+    tweetArray.sort(function(tweet1, tweet2) {
+        //have to use param as variable to search on tweet object
+        //param will hold likes, retweets, or replies
+        //direction will sort tweets in ascending or descending order
+        if(direction === "asc") {
+            return tweet1[param] - tweet2[param];
+        } else if(direction === "desc") {
+            return tweet2[param] - tweet1[param];
+        }
+    });
+    return tweetArray;
+}
 
 const reducer = (state = initialState, action) => {
     // Handle actions here - make sure you don't mutate the state!
-    const { type } = action;
+    const { type, param, direction } = action;
 
     // ACTION 1 - Sort by # likes
+    if(type === 'SORT' && param === 'likes' ) {
+        let newState = sortTweets(state, param, direction);
+        return newState;
+    }
 
     // ACTION 2 - Sort by # retweets
+    if(type === 'SORT' && param === 'retweets' ) {
+        let newState = sortTweets(state, param, direction);
+        return newState;
+    }
 
     // ACTION 3 - Sort by # replies
+    if(type === 'SORT' && param === 'replies' ) {
+        let newState = sortTweets(state, param, direction);
+        return newState; 
+    }
     
     return state;
 }
