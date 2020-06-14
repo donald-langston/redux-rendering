29b637cd0d4c8e70f9a03d2e7ac062075a49ef18
Donald <donald_langston@yahoo.com>
2020-06-14 14:17:52 -0400

    Updated index.js and reducer.js to generate random playing cards

--- poker/index.js
+++ poker/index.js
@@ -7,3 +7,27 @@ dispatch({ type: null }); // Here we're making a call to dispatch() - this trigg
 
 // Write DOM event listeners here, make them dispatch actions to the Redux store 
 
+let card1Btn = document.getElementById('card1Button');
+let card2Btn = document.getElementById('card2Button');
+let suits = ['C', 'D', 'H', 'S'];
+let values = [
+    '2', '3', '4', '5', '6',
+    '7', '8', '9', '10','A', 
+    'J', 'K', 'Q'
+];
+
+let randomSuit = function() {
+    return Math.floor(Math.random() * 4);
+}
+
+let randomValue = function() {
+    return Math.floor(Math.random() * 13);
+}
+
+card1Btn.addEventListener('click', () => {
+    dispatch({type: "RANDOM", suit: suits[randomSuit()], value: values[randomValue()], card: "card1"});
+});
+
+card2Btn.addEventListener('click', () => {
+    dispatch({type: "RANDOM", suit: suits[randomSuit()], value: values[randomValue()], card: "card2"});
+});
--- poker/reducer.js
+++ poker/reducer.js
@@ -11,9 +11,25 @@ const initialState = [
 
 const reducer = (state = initialState, action) => {
     // Handle actions here - make sure you don't mutate the state!
-    const { type } = action;
+    const { type, suit, value, card } = action;
 
     // ACTION - Change either card to a random new one (Try implementing one action that both buttons dispatch)
-    
+    if(type === "RANDOM" && card === "card1") {
+        let newState = [
+            ...state
+        ];
+        newState[0].value = value;
+        newState[0].suit = suit;
+        return newState;
+    }
+
+    if(type === "RANDOM" && card === "card2") {
+        let newState = [
+            ...state
+        ];
+        newState[1].value = value;
+        newState[1].suit = suit;
+        return newState;
+    }
     return state;
 }
